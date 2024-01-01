eb42dca30ee8a640ef0e8e66c727fd371331833b
anshuman7negi <anshumannegi777@gmail.com>
2024-01-01 12:29:19 +0530

    pused code

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://127.0.0.1:5173'
+    origins 'https://samyati.onrender.com'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
