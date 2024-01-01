156fa37bc932e51d3007a2b1cb6f9c0c0fa5095a
anshuman7negi <anshumannegi777@gmail.com>
2024-01-01 13:19:30 +0530

    last time

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,6 @@ module SamyatiBackend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+
   end
 end
