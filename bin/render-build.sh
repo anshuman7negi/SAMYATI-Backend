4269decf235ce2dce5e414ed6657d368ae39305f
anshuman7negi <anshumannegi777@gmail.com>
2024-01-02 10:22:27 +0530

    fixed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,20 @@
 # exit on error
 set -o errexit
 
+# Install gems
 bundle install
-bundle exec rails assets:precompile
+
+# Preinstall assets using esbuild
+bundle exec rails assets:preinstall
+
+# Clean up old assets (optional)
 bundle exec rails assets:clean
+
+# Migrate the database
 bundle exec rails db:migrate
+
+# Additional deployment steps if needed
+# ...
+
+# Start your Rails server or whatever is necessary for your deployment
+# ...
